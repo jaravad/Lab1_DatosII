3e312566f57b65a897a80c872a5008eed04595e6
jesu2607 <jaravad@uninorte.edu.co>
2018-08-29 20:24:28 -0500

    first commit

---
+++ src/lab1_jesus_santiago/AVL.java
@@ -0,0 +1,17 @@
+
+package lab1_jesus_santiago;
+
+/**
+ *
+ * @author Jesus David
+ */
+public class AVL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
